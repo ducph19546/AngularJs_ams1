bd858f55554245e332174c8d875789b4dc548b83
ducnguyen1002 <ducduc1022000@gmail.com>
2023-05-30 12:13:14 +0700

    clear changed dir

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { ProductsListComponent } from './pages/products-list/products-list.component';
-import { ProductDetailPageComponent } from './pages/product-detail-page/product-detail-page.component';
-import { ProductAddFormComponent } from './pages/product-add-form/product-add-form.component';
-import { ProductUpdateFormComponent } from './pages/product-update-form/product-update-form.component';
-import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+
 
 const routes: Routes = [
-  { path: "", component: HomePageComponent },
-  { path: "products", component: ProductsListComponent },
-  { path: "products/add", component: ProductAddFormComponent },
-  { path: "products/:id", component: ProductDetailPageComponent },
-  { path: "products/:id/update", component: ProductUpdateFormComponent },
-  { path: "**", component: NotFoundPageComponent }
+  
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,22 +6,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
-import { ProductsListComponent } from './pages/products-list/products-list.component';
-import { ProductDetailPageComponent } from './pages/product-detail-page/product-detail-page.component';
-import { ProductAddFormComponent } from './pages/product-add-form/product-add-form.component';
-import { ProductUpdateFormComponent } from './pages/product-update-form/product-update-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent,
-    NotFoundPageComponent,
-    ProductsListComponent,
-    ProductDetailPageComponent,
-    ProductAddFormComponent,
-    ProductUpdateFormComponent
+    
   ],
   imports: [
     BrowserModule,
